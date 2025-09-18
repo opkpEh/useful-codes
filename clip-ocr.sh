95d9d89063a9de222e50da38031c69aecb9b159f
opkpEh <harshpandeykp20@gmail.com>
2025-09-18 13:29:53 +0530

    ai answers

--- clip-ocr.sh
+++ clip-ocr.sh
@@ -3,3 +3,18 @@
 text=$(xclip -selection clipboard -t image/* -o | magick - png:- | tesseract stdin stdout 2>/dev/null)
 
 echo -n "$text" | xclip -selection clipboard
+
+output=""
+
+for (( i=0; i<${#text}; i++ ));
+do
+	char="${text:$i:1}"
+	if [[ "$char" == " " ]]
+	then
+		output="${output}%20"
+	else
+		output="${output}${char}"
+	fi
+done
+
+curl -X GET '[add your AI api]/$output'
